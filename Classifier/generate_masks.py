f39014363ab6631ee2dc6599347de876fa15f373
Jerrick Liu <jerrick.y.liu@gmail.com>
2019-07-16 15:59:38 -0400

    Updated imwrite features

--- Classifier/generate_masks.py
+++ Classifier/generate_masks.py
@@ -6,17 +6,15 @@ import fnmatch
 
 
 
-def mask(path = '/home/jerrick/Documents/Projects/For_Jerrick/data/all_sand/sand_0/2S1'):
+def mask(path = '/Users/SirJerrick/Downloads/images'):
     i = 0
     for root, dirs, filename in os.walk(path):
         for file in filename:
-            if fnmatch.fnmatch(file, '*.png'):
-                new_path = os.path.join(root, file)
-                img = cv2.imread(new_path)
-                gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-                i += 1
-                ret, thresh = cv2.threshold(gray_img, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
-                cv2.imwrite('/home/jerrick/Documents/Projects/For_Jerrick/data/all_sand/sand_0/2S1_masks/2S1_mask{}'.format(i))
-                cv2.imshow('image', thresh)
-                cv2.waitKey(0)
-                cv2.destroyAllWindows()
+            new_path = os.path.join(root, file)
+            img = cv2.imread(new_path)
+            gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+            i += 1
+            ret, thresh = cv2.threshold(gray_img, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
+            cv2.imwrite('/Users/SirJerrick/Downloads/masks/apple_mask{}.png'.format(i), thresh)
+
+mask()

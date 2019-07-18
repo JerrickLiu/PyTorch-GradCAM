31f2666474e88e671bda040977f81e39ac8b0310
Jerrick Liu <31054052+SirJerrick@users.noreply.github.com>
2019-07-18 10:50:09 -0400

    Added two new functions to apply masks to original image and generate a random colored background

--- Classifier/generate_masks.py
+++ Classifier/generate_masks.py
@@ -7,14 +7,42 @@ import fnmatch
 
 
 def mask(path = '/Users/SirJerrick/Downloads/images'):
-    i = 0
     for root, dirs, filename in os.walk(path):
         for file in filename:
             new_path = os.path.join(root, file)
             img = cv2.imread(new_path)
             gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-            i += 1
-            ret, thresh = cv2.threshold(gray_img, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
-            cv2.imwrite('/Users/SirJerrick/Downloads/masks/apple_mask{}.png'.format(i), thresh)
+            ret, thresh = cv2.threshold(gray_img, 20, 255, cv2.THRESH_BINARY_INV)
+            f, e = os.path.splitext(new_path)
+            cv2.imwrite(f + 'Masked.png', thresh)
 
-mask()
+def final_image(path = '/same/path/where/you/generated/the/masks'):
+    for root, dirs, filename in os.walk(path):
+        for file in sorted(filename):
+            if fnmatch.fnmatch(file, '*.png') and not fnmatch.fnmatch(file, '*Masked.png'):
+                new_path = os.path.join(root, file)
+                img = cv2.imread(new_path)
+            if fnmatch.fnmatch(file, '*Masked.png'):
+                mask_path = os.path.join(root, file)
+                mask = cv2.imread(mask_path)
+                out = mask.copy()
+                out[mask == 255] = img[mask == 255]
+                f, e = os.path.splitext(mask_path)
+                cv2.imwrite(f + 'MaskedFinal.png', out)
+
+def generate_colored_background(path = '/same/path'):
+    for root, dirs, filename in os.walk(path):
+        for file in filename:
+            if fnmatch.fnmatch(file, '*MaskedFinal.png'):
+                new_path = os.path.join(root, file)
+                color = list(np.random.choice(range(256), size=3))
+                img = cv2.imread(new_path)
+                img[np.where(((img == [0, 0, 0]).all(axis = 2)))] = color
+                f, e = os.path.splitext(new_path)
+                cv2.imwrite(f + 'ColoredBG.png', img)
+def main():
+    mask()
+    #final_image()
+    #generate_colored_background()
+if __name__ == '__main__':
+    main()

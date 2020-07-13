34f2dbc8d9171167e547793f1bb7a0cd2e8a058f
Jerrick Liu <31054052+JerrickLiu@users.noreply.github.com>
2020-07-13 00:06:43 -0400

    Update and rename moving_items to moving_items.py

--- Classifier/moving_items.py
+++ Classifier/moving_items.py
@@ -3,18 +3,31 @@ import os
 import fnmatch
 from PIL import Image
 
-def moving_folders_with_specific_name(cur_dir = '/home/jerrick/Documents/Projects/For_Jerrick/data/classes',
-                                      dest = '/home/jerrick/Documents/Projects/For_Jerrick/data/all_4in_asphalt/'):
+def moving_folders_with_specific_name(cur_dir, dest):
+    """ Parameters
+        
+        cur_dir = path where your folders want moved
+        
+        dest = path to destination folder
+    
+    """
     count = 0
     for root, dirs, filename in os.walk(cur_dir):
         for dir in dirs:
-            if dir.startswith('ZIL131'):
+            if dir.startswith('name_you_want'):
                 dest_folder = os.path.join(dest, dir + '_' + str(count))
                 count += 1
                 shutil.copytree(os.path.join(root, dir), dest_folder, symlinks=True)
 
-def moving_files_with_specific_end(data_dir = '/home/jerrick/Documents/Projects/For_Jerrick/data/combined_aug_all_resolutions/train/HMMWV',
-                                   dest = '/home/jerrick/Documents/Projects/For_Jerrick/data/combined_aug_all_resolutions/train/train/HMMWV'):
+def moving_files_with_specific_end(data_dir, dest):
+                                   
+     """ Parameters
+        
+        cur_dir = path where your folders want moved
+        
+        dest = path to destination folder
+        
+     """
 
     for root, dirs, filename in os.walk(data_dir):
         for files in filename:
